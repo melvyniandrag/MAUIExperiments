20774a893e8a0135a48d1c2e05b827072b209452
Melvyn Drag Laptop <melvyniandrag@gmail.com>
2024-09-30 16:43:46 -0400

    binding works!

--- AbsoluteLayoutExperiments/CircleLayout.xaml.cs
+++ AbsoluteLayoutExperiments/CircleLayout.xaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace AbsoluteLayoutExperiments;
 
@@ -7,7 +8,14 @@ public partial class CircleLayout : AbsoluteLayout
 
     public static readonly BindableProperty ButtonsProperty =
     BindableProperty.Create(nameof(Buttons), typeof(ObservableCollection<Button>), typeof(Button), new ObservableCollection<Button>(){new Button(), new Button(), new Button()},
-        propertyChanged: (bindable, oldVal, newVal) => { ((CircleLayout)bindable).DrawACircle();  });
+        propertyChanged: (bindable, oldVal, newVal) => {
+            foreach(var button in (ObservableCollection<Button>)newVal)
+            {
+                button.Command = ((CircleLayout)bindable).ButtonClickedCommand;
+                button.CommandParameter = button;
+            }
+            ((CircleLayout)bindable).DrawACircle();  
+        });
 
     public ObservableCollection<Button> Buttons
     {
@@ -21,14 +29,36 @@ public partial class CircleLayout : AbsoluteLayout
         }
     }
 
-     void DrawACircle()
+    public static readonly BindableProperty ButtonClickedCommandProperty  =
+        BindableProperty.Create(nameof(ButtonClickedCommand), typeof(ICommand), typeof(CircleLayout), 
+            propertyChanged : (bindable, oldVal, newVal) => {
+                foreach (var button in ((CircleLayout)bindable).Buttons)
+                {
+                    button.Command = ((CircleLayout)bindable).ButtonClickedCommand;
+                    button.CommandParameter = button;
+                }
+            });
+
+    public ICommand ButtonClickedCommand
+    {
+        get
+        {
+            return (ICommand)GetValue(ButtonClickedCommandProperty);
+        }
+        set
+        {
+            SetValue(ButtonClickedCommandProperty, value);
+        }
+    }
+
+    void DrawACircle()
     {
         this.Clear();
         double x = 1;
         foreach(Button btn in Buttons)
         {
             this.Add(btn);
-            this.SetLayoutBounds(btn, new Microsoft.Maui.Graphics.Rect(x, 2, 60, 200));
+            this.SetLayoutBounds(btn, new Microsoft.Maui.Graphics.Rect(x, 2, 200, 60));
             x += 220;
         }
     }
--- AbsoluteLayoutExperiments/MainViewModel.cs
+++ AbsoluteLayoutExperiments/MainViewModel.cs
@@ -17,11 +17,25 @@ namespace AbsoluteLayoutExperiments
         ObservableCollection<Button> buttons =
         [
             new Button(){ Text="congrats"},
-            new Button(){ Text="you did it"}
+            new Button(){ Text="you did it"},
+            new Button()
         ];
 
         [ObservableProperty]
         string myText = "hello";
+
+
+        private void ButtonClickedHandler(object sender, EventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("you clicked " + ((Button)sender).Text);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
 
         [RelayCommand]
         void OnButtonClicked(Button button)
